7bd27cec553a0421c98b5abda5dcca1b5df38455
Khadija Hawa <khadijahawa555@gmail.com>
2023-01-03 20:31:25 +0300

    edited on assign to a developer

--- src/components/main/AddProject.js
+++ src/components/main/AddProject.js
@@ -1,73 +1,152 @@
-import React, { useState } from 'react';
-import db from '../../firebaseConfig';
-import { Form, Button, Col, Row, Image, ListGroup, ListGroupItem } from 'react-bootstrap';
-import image from '../images/add.jpg';
+import React, { useState, useEffect } from "react";
+import db from "../../firebaseConfig";
+import {
+  Form,
+  Button,
+  Col,
+  Row,
+  Image,
+  ListGroup,
+  ListGroupItem,
+  Dropdown,
+  DropdownButton,
+} from "react-bootstrap";
+import image from "../images/add.jpg";
 
 function AddProject() {
-
   const [formData, setFormData] = useState({
-    title: '',
-    description: '',
-    deadline: '',
-    task: []
+    title: "",
+    description: "",
+    deadline: "",
+    task: [],
+    developer: "",
+  });
+  const [developer, setDeveloper] = useState([]);
+  const [taskData, setTaskData] = useState("");
+
+  useEffect(() => {
+    async function fetchDevelopers() {
+      const arrOfDeveloperNames = [];
+
+      await db
+        .collection("developer")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            arrOfDeveloperNames.push({ id: doc.id, ...doc.data() });
+          });
+        });
+      await setDeveloper(arrOfDeveloperNames);
+    }
+    fetchDevelopers();
   });
 
-  const [taskData, setTaskData] = useState('');
-
-  function handleChange(e){
+  function handleChange(e) {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   }
 
-  function handleTask(e){
+  function handleTask(e) {
     e.preventDefault();
     console.log(formData);
     setFormData({
-      ...formData, 
-      task: [...formData.task, taskData]
+      ...formData,
+      task: [...formData.task, taskData],
     });
-    setTaskData('');
+    setTaskData("");
   }
 
-  function handleProject(e){
+  function handleProject(e) {
     e.preventDefault();
-    db.collection('projects').add(formData);
-    setFormData({title: '', description: '', deadline: '', task: []});
+    db.collection("projects").add(formData);
+    setFormData({ title: "", description: "", deadline: "", task: [] });
   }
-  
+
   return (
-    <div className='addProjectPage'>
-      <div style={{marginRight: '2%'}}>
-        <Image src={image} roundedCircle className='loginImage' />
+    <div className="addProjectPage">
+      <div style={{ marginRight: "2%" }}>
+        <Image src={image} roundedCircle className="loginImage" />
       </div>
-      <div style={{width: '50%'}}>
+      <div style={{ width: "50%" }}>
         <Form onSubmit={handleProject}>
+          <DropdownButton
+            id="dropdown-basic-button"
+            title="Assign To A Developer"
+            style={{ margin: 5, padding: 2 }}
+          >
+            {developer.map((name) => {
+              return (
+                <Dropdown.Item href="#/action-1">{name.username}</Dropdown.Item>
+              );
+            })}
+          </DropdownButton>
+
           <Form.Group controlId="formBasic">
             <Form.Label>Title</Form.Label>
-            <Form.Control type="text" placeholder="Title" name='title' value={formData.title} onChange={handleChange} required />
+            <Form.Control
+              type="text"
+              placeholder="Title"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+              required
+            />
           </Form.Group>
           <Form.Group controlId="exampleForm.ControlTextarea1">
             <Form.Label>Description</Form.Label>
-            <Form.Control as="textarea" name='description' value={formData.description} onChange={handleChange} rows={3} required />
+            <Form.Control
+              as="textarea"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              rows={3}
+              required
+            />
           </Form.Group>
           <Form.Group controlId="formBasic">
             <Form.Label>Deadline</Form.Label>
-            <Form.Control type="date" name='deadline' value={formData.deadline} onChange={handleChange} required />
+            <Form.Control
+              type="date"
+              name="deadline"
+              value={formData.deadline}
+              onChange={handleChange}
+              required
+            />
           </Form.Group>
           <Row>
             {/* <Col auto><Form.Label htmlFor="floatingInput">Task</Form.Label></Col> */}
-            <Col md={10}><input className="form-control" id="floatingInput" type='text' name='task' placeholder='Task' value={taskData} onChange={(e) => setTaskData(e.target.value)}></input></Col>
-            <Col md='auto'><button className='btn btn-primary' onClick={handleTask}>Add Task</button></Col>
+            <Col md={10}>
+              <input
+                className="form-control"
+                id="floatingInput"
+                type="text"
+                name="task"
+                placeholder="Task"
+                value={taskData}
+                onChange={(e) => setTaskData(e.target.value)}
+              ></input>
+            </Col>
+            <Col md="auto">
+              <button className="btn btn-primary" onClick={handleTask}>
+                Add Task
+              </button>
+            </Col>
           </Row>
           <br />
           <Col>
             <ListGroup>
-              {formData.task && formData.task.map(task => <ListGroupItem>{task}</ListGroupItem>)}
-            </ListGroup> <br />
+              {formData.task &&
+                formData.task.map((task) => (
+                  <ListGroupItem>{task}</ListGroupItem>
+                ))}
+            </ListGroup>{" "}
+            <br />
           </Col>
-          <Button variant="outline-success" type="submit" size="lg" block>Add Project</Button>
+          <Button variant="outline-success" type="submit" size="lg" block>
+            Add Project
+          </Button>
         </Form>
       </div>
     </div>
--- src/components/main/Calender/index.js
+++ src/components/main/Calender/index.js
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from "react";
 import db from "../../../firebaseConfig";
 import SingleProjectPage from "../SingleProjectPage";
-import { Card, Container, Col, Row, Button } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 import moment from "moment";
 // import Calendar from "react-calendar";
 // import "react-calendar/dist/Calendar.css";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-function Calender({ loggedInUser, props }) {
+function Calender({ loggedInUser }) {
   const localizer = momentLocalizer(moment);
-
   const [projects, setProjects] = useState([]);
-  const [dateObject, setdateObject] = useState(moment());
-
-  const [isClicked, setIsClicked] = useState("");
-  const [showMonthTable, setShowMonthTable] = useState(false);
-  const [deadline, setDeadline] = useState(new Date());
 
   useEffect(() => {
     async function fetchProjects() {
@@ -30,78 +24,77 @@ function Calender({ loggedInUser, props }) {
             arrOfProjects.push({ id: doc.id, ...doc.data() });
           });
         });
-      console.log("arrOfProjects", arrOfProjects);
       await setProjects(arrOfProjects);
     }
     fetchProjects();
   });
 
-  const onChange = (deadline) => {
-    setDeadline(deadline);
-  };
+  if (loggedInUser === "")
+    return (
+      <Container>
+        <h1>Please sign-in in order to see this page</h1>
+      </Container>
+    );
+  else
+    return (
+      <Container className="container projectsPage" fluid="md">
+        <div
+          style={{
+            fontSize: "1.5rem",
+            textAlign: "center",
+            margin: 9,
+            padding: 3,
+          }}
+        >
+          <b>
+            <i>Deadlines Calender</i>
+          </b>
+        </div>
+        <Row className="justify-content-md-center">
+          {projects.map((project) => {
+            const handleSelectEvent = (project) => {
+              alert(project.title);
+            };
 
-  // if (loggedInUser === "")
-  //   return (
-  //     <Container>
-  //       <h1>Please sign-in in order to see this page</h1>
-  //     </Container>
-  //   );
-  // else
-  return (
-    <Container className="container projectsPage" fluid="md">
-      <div
-        style={{
-          fontSize: "1.5rem",
-          textAlign: "center",
-          margin: 9,
-          padding: 3,
-        }}
-      >
-        <b>
-          <i>Deadlines Calender</i>
-        </b>
-      </div>
-      <Row className="justify-content-md-center">
-        {isClicked !== "" && (
-          <SingleProjectPage
-            data={isClicked}
-            setIsClicked={setIsClicked}
-            loggedInUser={loggedInUser}
-          />
-        )}
-        {projects.map((project) => {
-          const handleSelectEvent = (project) => {
-            alert(project.title);
-          };
+            const events = [
+              {
+                start: new Date(),
+                end: project.deadline,
+                title: project.title,
+                description: project.description,
+              },
+            ];
 
-          const events = [
-            {
-              start: new Date(),
-              end: project.deadline,
-              title: project.title,
-              description: project.description,
-            },
-          ];
-
-          return (
-            <Col xs={12} md={4} key={project.title} className="gridSystem">
-              <div className="text-center"> Name: {project.title}</div>
-              <Calendar
-                localizer={localizer}
-                events={events}
-                views={["month"]}
-                style={{ height: 500 }}
-                onSelectEvent={handleSelectEvent}
-              />
-              <div>Title :{project.title}</div>
-              <div>Description :{project.description}</div>
-              <div>Deadline :{project.deadline}</div>
-            </Col>
-          );
-        })}
-      </Row>
-    </Container>
-  );
+            return (
+              <Col xs={12} md={4} key={project.title} className="gridSystem">
+                <div
+                  className="text-center"
+                  style={{
+                    fontSize: "1rem",
+                    textAlign: "center",
+                    margin: 9,
+                    padding: 3,
+                  }}
+                >
+                  {" "}
+                  Name: {project.title}
+                </div>
+                <Calendar
+                  localizer={localizer}
+                  events={events}
+                  views={["month"]}
+                  style={{ height: 500 }}
+                  onSelectEvent={handleSelectEvent}
+                />
+                <div>Title :{project.title}</div>
+                <div>Description :{project.description}</div>
+                <div>Deadline :{project.deadline}</div>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    );
 }
 
 export default Calender;
